1fef430a379cc314738c4a9d4911fc1ada32c55c
Saaketh <saakethaj@gmail.com>
2026-02-14 11:21:34 +0530

    Add abstraction

---
+++ 08. Oop/AbstractionLearn.py
@@ -0,0 +1,30 @@
+# * Abstraction
+# * Abstraction in Python, a core principle of Object-Oriented Programming (OOP), involves simplifying complex systems by hiding unnecessary
+# * implementation details and exposing only the essential functionalities to the user. 
+
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius ** 2
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+
+shapes = [Circle(5), Rectangle(4, 6)]
+for shape in shapes:
+    print(shape.area())
